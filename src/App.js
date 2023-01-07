584679fd70a4e748a07d6fba32511b6796f55ac2
sara ali <blgees77saba@gmail.com>
2023-01-07 09:41:57 +0200

    map

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br> 
+      </br>
+     <h1> checkpoint Google Map my location</h1>  
+      <iframe width="1000" height="700" id="g_map" title="map"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d479.52170871833397!2d14.441635544668056!3d27.033391288844335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13c6fb1dedae3c67%3A0x3b9d1a19589bed3d!2zMkNNUis5UjIsIFNhYmjEgQ!5e1!3m2!1sen!2sly!4v1673076765518!5m2!1sen!2sly" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}export default App;
